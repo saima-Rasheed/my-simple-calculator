75b037962b9c1b4bfdf49f47fc117502c414ec3b
saima-rasheed <saimaarasheed149@gmail.com>
2024-04-12 01:22:10 +0500

    push my claculator on github

---
+++ index.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const asnwer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "select one of the operatorction to perform action",
+        type: "list",
+        name: "operators",
+        choices: ["addition", "subtraction", "multiplication", "division"],
+    },
+]);
+//conditional statement
+if (asnwer.operators === "addition") {
+    console.log(asnwer.firstNumber + asnwer.secondNumber);
+}
+else if (asnwer.operators === "subtraction") {
+    console.log(asnwer.firstNumber - asnwer.secondNumber);
+}
+else if (asnwer.operators === "multiplication") {
+    console.log(asnwer.firstNumber * asnwer.secondNumber);
+}
+else if (asnwer.operators === "division") {
+    console.log(asnwer.firstNumber / asnwer.secondNumber);
+}
+else {
+    console.log("please select a valid operator");
+}
